47a5d738569ff5baa03490e7ba59b70f7907e67b
Sarah L. Smolen <ssmolen7@umd.edu>
2017-11-15 16:05:01 -0500

    Bulk of UI stuff working

--- WhosThere/app/src/main/java/com/example/sarah/whosthere/HomePage.java
+++ WhosThere/app/src/main/java/com/example/sarah/whosthere/HomePage.java
@@ -1,5 +1,6 @@
 package com.example.sarah.whosthere;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -55,20 +56,21 @@ public class HomePage extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
+        Intent i = new Intent();
         if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_connect) {
-
+            i = new Intent(this, HomePage.class);
         } else if (id == R.id.nav_friends) {
-
+            i = new Intent(this, FriendsList.class);
         } else if (id == R.id.nav_groups) {
 
         } else if (id == R.id.nav_settings) {
-
+            i = new Intent(this, SettingsActivity.class);
         } else if (id == R.id.nav_logout) {
 
         }
+        i.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+        startActivity(i);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
